b2b177f59ae5421117d123f9468f9deec2c0ac16
Leo Hayes <leo.m.hayes@hotmail.co.uk>
2026-01-23 02:07:30 +0000

    Starting on build_Table.py with initialising observed values

--- src/build_Table.py
+++ src/build_Table.py
@@ -3,6 +3,13 @@ import json
 import datetime
 import pandas as pd
 
+DATASETS = {
+    "observed_kp":"dayind",
+    "geomag_forecast": "geomag_forecast",
+    "3day_forecast": "3day_forecast",
+    "omni2":"omni2"
+}
+
 def importFile(file_path:str):
     try:
         with open(file_path, "r", encoding="utf-8") as f:
@@ -11,12 +18,27 @@ def importFile(file_path:str):
     except FileNotFoundError:
         raise ValueError(f"JSON file not found at: {file_path}.")
 
+def extractObservedKp():
+    pass
+
+def extractGeomagForecast():
+    pass
+
+def extract3dayForecast():
+    pass
+
+def extractOmni2():
+    pass
+
+
 def buildForecast(data:dict):
 
 
     pass
 
 def buildObserved():
+    # import observed day
+    
     pass
 
 def buildOMNI():
@@ -25,13 +47,36 @@ def buildOMNI():
 def buildDF():
     pass
 
+
+def getProcDatapath(base:str, dataset_key: str):
+    try:
+        return os.path.join(base, "data/data_processed", DATASETS[dataset_key])
+
+    except:
+        raise ValueError(f"Unknown dataset key: {dataset_key}")
+
 def main():
     base = os.environ.get("SPIDER")
     if base is None:
         raise EnvironmentError("SPIDER system variable is not set!")
     
-    proc_path = "data/data_processed/"
-    data_names = ["3day_forecast", "dayind", "geomag_forecast", "omni2"]
+    observed_data = {}
+    observed_path = getProcDatapath(base, "observed_kp")
+    years = sorted(d for d in os.listdir(observed_path) 
+                   if os.path.isdir(os.path.join(observed_path, d)))
+    for year in years:
+        year_path = os.path.join(observed_path, year)
+
+        for file_name in sorted(os.listdir(year_path)):
+            if not file_name.endswith(".json"):
+                continue
+                
+            filepath = os.path.join(year_path, file_name)
+            observed_json = importFile(filepath)
+
+            rows = extractObservedKp()
+
+    print("STOP!")
 
 if __name__ == "__main__":
     main()
