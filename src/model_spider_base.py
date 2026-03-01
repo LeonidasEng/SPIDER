e546353387419c9ecd05d4b5b73094305e1ce57f
Leo Hayes <leo.m.hayes@hotmail.co.uk>
2026-03-01 13:43:49 +0000

    Added NB and LR baseline models, changed climatology to fit to train data and apply to test, matching new models

--- src/model_spider_base.py
+++ src/model_spider_base.py
@@ -3,39 +3,39 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import metrics
-from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
 
 TARGETS = {
         "3 Day Forecast 0030": "spider_targets_3day_0030.parquet",
-        "3 Day Forecast 1230": "spider_targets_3day_1230.parquet",
-        #"Geomag Forecast": "spider_targets_geomag.parquet"
+        "3 Day Forecast 1230": "spider_targets_3day_1230.parquet"
     }
 
 # Create Test and Train datasets: https://www.geeksforgeeks.org/python/pandas-create-test-and-train-samples-from-dataframe/
 # I'll add later when I need for NB and LR
 
-def dataSplit(df:pd.DataFrame, dataset:str):
-    # Split into 70-30 split
-    df = df.sort_index()
+def dataSplit(df: pd.DataFrame, dataset:str):
+    '''
+    Chronological split (not percentage split):
+    Train: November 2012 -> December 2022
+    Test: January 2023 -> December 2025
+    '''
+    df = df.sort_values(["issue_time_utc", "valid_start_utc"])
 
-    split_idx = int(len(df) * 0.7) # 70% of the data
+    cutoff = pd.Timestamp("2023-01-01", tz="UTC")
 
-    train_set = df[:split_idx] # Up to 70%
-    test_set = df[split_idx:] # Remaining 30%
+    train_set = df[df["issue_time_utc"] < cutoff]
+    test_set = df[df["issue_time_utc"] >= cutoff]
 
     print(f"{dataset}")
-    print(f"Train: {train_set.shape}, Test: {test_set.shape}")
+    print(f"Train: {train_set.shape} Test: {test_set.shape}")
     return train_set, test_set
 
-def persistenceBase(df:pd.DataFrame, lead_day:int, target_col="is_large_error"):
+def persistenceBase(df:pd.DataFrame, target_col="is_large_error"):
     '''
     Was forecast wrong today? Will it be wrong tomorrow?
     '''
     df = df.copy()
-
-    df = df[df["lead_day"] == lead_day]
-
     df["probability"] = df[target_col].shift(1)
 
      # Remove missing values
@@ -43,56 +43,111 @@ def persistenceBase(df:pd.DataFrame, lead_day:int, target_col="is_large_error"):
 
     y_true = df[target_col].astype(int)         # Ground Truth
     y_prob = df["probability"].astype(float)    # Deterministic 0 or 1 but treating as prob for Brier and ROC
-    y_pred = (y_prob == 1).astype(int)        # Predicted binary classification
-
-    # accuracy = (y_true == y_pred).mean()
-    # print(f"Persistence accuracy {accuracy: .3f}")
+    y_pred = (y_prob == 1).astype(int)          # Predicted binary classification
 
     return y_true, y_prob, y_pred
 
-def climatologyBase(df:pd.DataFrame, lead_day:int, target_col="is_large_error", forecast_col="kp_forecast"):
-    df = df.copy()
 
-    df = df[df["lead_day"] == lead_day]
+def climatologyFit(train_set:pd.DataFrame,
+                   target_col="is_large_error", forecast_col="kp_forecast"):
+    train = train_set.copy()
+    train = train.dropna(subset=[forecast_col, target_col])
 
-    # # Using Data Wrangler, I can see the distribution of OMNI data.
-    # # Using only 2 drivers to maintain broad outlook (I think!)
-    # bins_bz = [-np.inf, -20, -10, -5, 0, 5, np.inf] 
-    # # [-inf, -20], [-20, -10], [-10, -5], [-5, 0], [0, 5], [5, inf]
-    # bins_vsw = [0, 350, 450, 600, 800, np.inf] 
-    # # [0, 350], [350, 450], [450, 600], [600, 800], [800, inf]
-
-    # df["bz_bin"]  = pd.cut(df["bz_gsm"], bins=bins_bz, include_lowest=True) # Include lowest for Large -Bz = Storm
-    # df["vsw_bin"] = pd.cut(df["v_sw"], bins=bins_vsw)
+    # Bin Kp for Climatology to reduce noise
+    train["kp_bin"] = train[forecast_col].round().clip(0, 9).astype(int)
 
     # Climatology-based probabilty model P(|ΔKp| > 1)| Forecast Kp)
-    climatology = (
-        df.groupby(forecast_col)[target_col]
+    clim_map = (
+        train.groupby("kp_bin", observed=True)[target_col]
         .mean()
-        .rename("probability")
-        .reset_index()
+        .to_dict()
     )
 
-    df = df.merge(climatology, on=forecast_col, how="left")
-    
-    y_true = df[target_col].astype(int)
-    y_prob = df["probability"].fillna(df[target_col].mean()) # Fill missing with zeros
-    y_pred = (y_prob >= 0.5).astype(int) # baseline prediction threshold (Camporeale:2025) 
-    # Not: 0.15, 0.2, 0.3, 0.35 (T0), 0.4, 0.45
-    # Based on Camporeale I should use the y_pred that maximises TSS.
-    # 0.35 3 Day Forecast 0030,Climatology,0,0.63,0.46,0.5,0.48,0.21,0.63,0.5,0.54,0.32,0.19,0.19
-    # accuracy = (y_true == y_pred).mean()
-    # print(f"Climatology accuracy: {accuracy: .3f}")
+    global_mean = float(train[target_col].mean())
+    return clim_map, global_mean
+
+def climatologyApply(test_set:pd.DataFrame,
+                     clim_map: dict, global_mean: float, 
+                     target_col="is_large_error", forecast_col="kp_forecast"):
+    test = test_set.copy()
+    test = test.dropna(subset=[forecast_col, target_col])
+
+    test["kp_bin"] = test[forecast_col].round().clip(0, 9).astype(int)
+    test["probability"] = test["kp_bin"].map(clim_map).fillna(global_mean)
+
+    y_true = test[target_col].astype(int)
+    y_prob = test["probability"].astype(float)
+    y_pred = (y_prob >= 0.5).astype(int) # baseline prediction threshold (Camporeale:2025)
 
     return y_true, y_prob, y_pred
 
-def gaussianBase(df:pd.DataFrame, lead_day:int):
-    df = df.copy()
+def gaussianBase(train_set:pd.DataFrame, test_set:pd.DataFrame):
+    '''
+    Gaussian Naive Bayes Baseline Model
+    '''
+    feature_columns = [
+        "bz_gsm",
+        "b_mag",
+        "v_sw",
+        "np",
+        "pdyn",
+        "ey",
+        "beta",
+        "mach_alfven",
+        "f10.7"
+    ]
 
-    df = df[df["lead_day"] == lead_day]
+    df_train = train_set.dropna(subset=feature_columns + ["is_large_error"])
+    df_test = test_set.dropna(subset=feature_columns + ["is_large_error"])
 
-    clf = GaussianNB()
-    pass
+    X_train = df_train[feature_columns]
+    y_train = df_train["is_large_error"]
+
+    X_test = df_test[feature_columns]
+    y_test = df_test["is_large_error"]
+
+    nb = GaussianNB()
+    nb.fit(X_train, y_train)
+
+    y_true = y_test
+    y_prob = nb.predict_proba(X_test)[:, 1]
+    y_pred = (y_prob >= 0.5).astype(int)
+    
+    return y_true, y_prob, y_pred
+
+def logisticBase(train_set:pd.DataFrame, test_set:pd.DataFrame):
+    '''
+    Logistic Regression Baseline model
+    '''
+    feature_columns = [
+        "bz_gsm",
+        "b_mag",
+        "v_sw",
+        "np",
+        "pdyn",
+        "ey",
+        "beta",
+        "mach_alfven",
+        "f10.7"
+    ]
+
+    df_train = train_set.dropna(subset=feature_columns + ["is_large_error"])
+    df_test = test_set.dropna(subset=feature_columns + ["is_large_error"])
+
+    X_train = df_train[feature_columns]
+    y_train = df_train["is_large_error"]
+
+    X_test = df_test[feature_columns]
+    y_test = df_test["is_large_error"]
+
+    lr = LogisticRegression(max_iter=1000, solver="lbfgs", class_weight="balanced")
+    lr.fit(X_train, y_train)
+
+    y_true = y_test
+    y_prob = lr.predict_proba(X_test)[:, 1]
+    y_pred = (y_prob >= 0.5).astype(int)
+
+    return y_true, y_prob, y_pred
 
 def cmDisplay(observed, predicted):
     
@@ -131,7 +186,7 @@ def metricsTable(dataset, lead_day, model_name, y_true, y_prob, y_pred):
         "Precision": round(precision, 2),
         "Recall": round(recall, 2),
         "F1": round(f1, 2),
-        "Brier": round(brier, 2) ,
+        "Brier": round(brier, 2),
         "AUC": round(auc, 2),
         "POD": round(pod, 2), # Same as recall, use as verification but delete in final version
         "FAR": round(far, 2),
@@ -147,15 +202,14 @@ def main():
         raise EnvironmentError("SPIDER system variable is not set!")
     
     dataset_path = os.path.join(base, "data", "datasets")
-    
+
     train_sets = {}
     test_sets = {}
     tables = {}
 
     kp_column = {
             "3 Day Forecast 0030": "kp_threeday",
-            "3 Day Forecast 1230": "kp_threeday",
-            #"Geomag Forecast": "kp_geomag"
+            "3 Day Forecast 1230": "kp_threeday"
         }
     
     for dataset, file_name in TARGETS.items():
@@ -166,17 +220,34 @@ def main():
         train_sets[dataset] = train_set
         test_sets[dataset]  = test_set
         tables[dataset] = {}
-        
+
+        train_set = train_set.rename(columns={kp_column[dataset]: "kp_forecast"})
+        test_set = test_set.rename(columns={kp_column[dataset]: "kp_forecast"})
+
         for lead_day in [0, 1, 2]:
             rows = []
+
+            df_ld = df[df["lead_day"] == lead_day].copy()
+            df_ld = df_ld.sort_values(["issue_time_utc", "valid_start_utc"])
+
             print(f"Dataset: {dataset}")
-            y_true, y_prob,  y_pred = persistenceBase(test_set, lead_day)
-            print(f"{y_true.shape}, {y_pred.shape}")
+            print(f"Running Persistence baseline model for Lead Day {lead_day}...")
+            y_true, y_prob, y_pred = persistenceBase(test_set)
             rows.append(metricsTable(dataset, lead_day, "Persistence", y_true, y_prob, y_pred))
-            test_set = test_set.rename(columns={kp_column[dataset]: "kp_forecast"})
-            y_true, y_prob, y_pred = climatologyBase(test_set, lead_day)
-            print(f"{y_true.shape}, {y_pred.shape}")
+            
+            print(f"Running Climatology baseline model for Lead Day {lead_day}...")
+            clim_map, global_mean = climatologyFit(train_set) # Fit to train data
+            y_true, y_prob, y_pred = climatologyApply(test_set, clim_map, global_mean) # Apply to test for no future leakage
             rows.append(metricsTable(dataset, lead_day, "Climatology", y_true, y_prob, y_pred))
+
+            print(f"Running Naive Bayes baseline model for Lead Day {lead_day}...")
+            y_true, y_prob, y_pred = gaussianBase(train_set, test_set)
+            rows.append(metricsTable(dataset, lead_day, "NB", y_true, y_prob, y_pred))
+            
+            print(f"Running Logistic Regression baseline model for Lead Day {lead_day}...")
+            y_true, y_prob, y_pred = logisticBase(train_set, test_set)
+            rows.append(metricsTable(dataset, lead_day, "LR", y_true, y_prob, y_pred))
+            
             #cmDisplay(y_true, y_pred)
             tables[dataset][lead_day] = pd.DataFrame(rows)
 
